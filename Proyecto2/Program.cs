ce15560eb37b442488f895bc7e0fe62efb8ae369
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-11-10 00:29:10 -0600

    Just changes on the file
    we add the  crud function

--- Proyecto2/Program.cs
+++ Proyecto2/Program.cs
@@ -1,15 +1,154 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Proyecto2
 {
     internal class Program
     {
+        private static string filePath = "Alumnos.txt";
+
         static void Main(string[] args)
         {
+            int opcion = 0;
+            do
+            {
+                Console.WriteLine("1. Ingresar");
+                Console.WriteLine("2. Eliminar");
+                Console.WriteLine("3. Modificar");
+                Console.WriteLine("4. Salir");
+                Console.WriteLine("5. Ver lista de alumnos");
+                Console.WriteLine("Ingrese una opcion");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+                switch (opcion)
+                {
+                    case 1:
+                        Ingresar();
+                        break;
+                    case 2:
+                        Eliminar();
+                        break;
+                    case 3:
+                        Modificar();
+                        break;
+                    case 4:
+                        Console.WriteLine("Gracias por usar el programa");
+                        break;
+                    case 5:
+                        LeerListado();
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida");
+                        break;
+                }
+            } while (opcion != 4);
+        }
+
+        static void Guardar(string nombre, string id, string punteo)
+        {
+            StreamWriter escribir = new StreamWriter(filePath, true);
+            escribir.WriteLine(nombre + "," + id + "," + punteo);
+            escribir.Close();
+        }
+
+        static void Leer()
+        {
+            StreamReader leer = new StreamReader(filePath, true);
+            string linea = "";
+            while (linea != null)
+            {
+                linea = leer.ReadLine();
+                if (linea != null)
+                {
+                    string[] datos = linea.Split(',');
+                    Console.WriteLine("Nombre: " + datos[0]);
+                    Console.WriteLine("ID: " + datos[1]);
+                    Console.WriteLine("Punteo: " + datos[2]);
+                    Console.WriteLine(" ");
+                }
+            }
+            leer.Close();
+        }
+
+        static void Ingresar()
+        {
+            Console.WriteLine("Ingrese el nombre del alumno");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el ID del alumno");
+            string id = Console.ReadLine();
+            Console.WriteLine("Ingrese el punteo del alumno");
+            string punteo = Console.ReadLine();
+            Console.WriteLine("");
+            Guardar(nombre, id, punteo);
+        }
+
+        static void Eliminar()
+        {
+            Console.WriteLine("Ingrese el nombre del alumno que desea eliminar");
+            string nombre = Console.ReadLine();
+            string[] lineas = File.ReadAllLines(filePath);
+            File.WriteAllText(filePath, "");
+
+            foreach (string linea in lineas)
+            {
+                string[] datos = linea.Split(',');
+                if (datos[0] != nombre)
+                {
+                    Guardar(datos[0], datos[1], datos[2]);
+                }
+            }
+
+            Console.WriteLine("Datos del alumno eliminados con éxito.");
+            Console.WriteLine("");
+        }
+
+        static void Modificar()
+        {
+            Console.WriteLine("Ingrese el ID del alumno que desea modificar");
+            string id = Console.ReadLine();
+            string[] lineas = File.ReadAllLines(filePath);
+            File.WriteAllText(filePath, "");
+
+            foreach (string linea in lineas)
+            {
+                string[] datos = linea.Split(',');
+                if (datos[1] == id)
+                {
+                    Console.WriteLine("Ingrese el nuevo ID del alumno");
+                    string nuevoid = Console.ReadLine();
+                    Console.WriteLine("Ingrese el nuevo nombre del alumno");
+                    string nuevonombre = Console.ReadLine();
+                    Console.WriteLine("Ingrese el nuevo punteo del alumno");
+                    string nuevopunteo = Console.ReadLine();
+                    Console.WriteLine("");
+                    Guardar(nuevonombre, nuevoid, nuevopunteo);
+                }
+                else
+                {
+                    Guardar(datos[0], datos[1], datos[2]);
+                }
+            }
+
+            Console.WriteLine("Datos del alumno con ID " + id + " modificados con éxito.");
+        }
+
+        static void LeerListado()
+        {
+            StreamReader leer = new StreamReader(filePath);
+            string linea = "";
+            while (linea != null)
+            {
+                linea = leer.ReadLine();
+                if (linea != null)
+                {
+                    string[] datos = linea.Split(',');
+                    Console.WriteLine("Nombre: " + datos[0]);
+                    Console.WriteLine("ID: " + datos[1]);
+                    Console.WriteLine("Punteo: " + datos[2]);
+                    Console.WriteLine(" ");
+                }
+            }
+            leer.Close();
         }
     }
 }
