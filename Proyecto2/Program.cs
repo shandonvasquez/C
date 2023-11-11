a1e772e158a723830aa5f39ff827e49adf96cdf9
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-11-10 22:25:27 -0600

    Updated the try and catch
    THis new updated  is able to handle to many erros with try, catch and if in order to improve the customer experience

--- Proyecto2/Program.cs
+++ Proyecto2/Program.cs
@@ -15,140 +15,208 @@ namespace Proyecto2
                 Console.WriteLine("1. Ingresar");
                 Console.WriteLine("2. Eliminar");
                 Console.WriteLine("3. Modificar");
-                Console.WriteLine("4. Salir");
-                Console.WriteLine("5. Ver lista de alumnos");
+                Console.WriteLine("4. Ver listado de alumnos");
+                Console.WriteLine("5. Salir");
                 Console.WriteLine("Ingrese una opcion");
-                opcion = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("");
-                switch (opcion)
+
+                try
                 {
-                    case 1:
-                        Ingresar();
-                        break;
-                    case 2:
-                        Eliminar();
-                        break;
-                    case 3:
-                        Modificar();
-                        break;
-                    case 4:
-                        Console.WriteLine("Gracias por usar el programa");
-                        break;
-                    case 5:
-                        LeerListado();
-                        break;
-                    default:
-                        Console.WriteLine("Opcion no valida");
-                        break;
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("");
+                    switch (opcion)
+                    {
+                        case 1:
+                            Ingresar();
+                            break;
+                        case 2:
+                            Eliminar();
+                            break;
+                        case 3:
+                            Modificar();
+                            break;
+                        case 4:
+                            LeerListado();
+                            break;
+                        case 5:
+                            Console.WriteLine("Gracias por usar el programa");
+                            break;
+                        default:
+                            Console.WriteLine("Opcion no valida");
+                            break;
+                    }
                 }
-            } while (opcion != 4);
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ingrese un número válido.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error inesperado: {ex.Message}");
+                }
+
+            } while (opcion != 5);
         }
 
         static void Guardar(string nombre, string id, string punteo)
         {
-            StreamWriter escribir = new StreamWriter(filePath, true);
-            escribir.WriteLine(nombre + "," + id + "," + punteo);
-            escribir.Close();
+            try
+            {
+                StreamWriter escribir = new StreamWriter(filePath, true);
+                escribir.WriteLine(nombre + "," + id + "," + punteo);
+                escribir.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error al guardar datos: {ex.Message}");
+            }
         }
 
         static void Leer()
         {
-            StreamReader leer = new StreamReader(filePath, true);
-            string linea = "";
-            while (linea != null)
+            try
             {
-                linea = leer.ReadLine();
-                if (linea != null)
+                StreamReader leer = new StreamReader(filePath, true);
+                string linea = "";
+                while (linea != null)
                 {
-                    string[] datos = linea.Split(',');
-                    Console.WriteLine("Nombre: " + datos[0]);
-                    Console.WriteLine("ID: " + datos[1]);
-                    Console.WriteLine("Punteo: " + datos[2]);
-                    Console.WriteLine(" ");
+                    linea = leer.ReadLine();
+                    if (linea != null)
+                    {
+                        string[] datos = linea.Split(',');
+                        Console.WriteLine("Nombre: " + datos[0]);
+                        Console.WriteLine("ID: " + datos[1]);
+                        Console.WriteLine("Punteo: " + datos[2]);
+                        Console.WriteLine(" ");
+                    }
                 }
+                leer.Close();
             }
-            leer.Close();
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error al leer datos: {ex.Message}");
+            }
         }
 
         static void Ingresar()
         {
-            Console.WriteLine("Ingrese el nombre del alumno");
-            string nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese el ID del alumno");
-            string id = Console.ReadLine();
-            Console.WriteLine("Ingrese el punteo del alumno");
-            string punteo = Console.ReadLine();
-            Console.WriteLine("");
-            Guardar(nombre, id, punteo);
+            try
+            {
+                Console.WriteLine("Ingrese el nombre del alumno");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese el ID del alumno");
+                string id = Console.ReadLine();
+                Console.WriteLine("Ingrese el punteo del alumno");
+                string punteo = Console.ReadLine();
+                Console.WriteLine("");
+
+                if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(id) || string.IsNullOrWhiteSpace(punteo))
+                {
+                    Console.WriteLine("No puede dejar ningún campo vacío.");
+                    Console.WriteLine("");
+                    Ingresar();
+                }
+                else
+                {
+                    Guardar(nombre, id, punteo);
+                    Console.WriteLine("Datos del alumno guardados con éxito.");
+                    Console.WriteLine("");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inesperado: {ex.Message}");
+            }
         }
 
         static void Eliminar()
         {
-            Console.WriteLine("Ingrese el nombre del alumno que desea eliminar");
-            string nombre = Console.ReadLine();
-            string[] lineas = File.ReadAllLines(filePath);
-            File.WriteAllText(filePath, "");
-
-            foreach (string linea in lineas)
+            try
             {
-                string[] datos = linea.Split(',');
-                if (datos[0] != nombre)
-                {
-                    Guardar(datos[0], datos[1], datos[2]);
-                }
-            }
+                Console.WriteLine("Ingrese el nombre del alumno que desea eliminar");
+                string nombre = Console.ReadLine();
+                string[] lineas = File.ReadAllLines(filePath);
+                File.WriteAllText(filePath, "");
 
-            Console.WriteLine("Datos del alumno eliminados con éxito.");
-            Console.WriteLine("");
+                foreach (string linea in lineas)
+                {
+                    string[] datos = linea.Split(',');
+                    if (datos[0] != nombre)
+                    {
+                        Guardar(datos[0], datos[1], datos[2]);
+                    }
+                }
+
+                Console.WriteLine("Datos del alumno eliminados con éxito.");
+                Console.WriteLine("");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error al eliminar datos: {ex.Message}");
+            }
         }
 
         static void Modificar()
         {
-            Console.WriteLine("Ingrese el ID del alumno que desea modificar");
-            string id = Console.ReadLine();
-            string[] lineas = File.ReadAllLines(filePath);
-            File.WriteAllText(filePath, "");
-
-            foreach (string linea in lineas)
+            try
             {
-                string[] datos = linea.Split(',');
-                if (datos[1] == id)
-                {
-                    Console.WriteLine("Ingrese el nuevo ID del alumno");
-                    string nuevoid = Console.ReadLine();
-                    Console.WriteLine("Ingrese el nuevo nombre del alumno");
-                    string nuevonombre = Console.ReadLine();
-                    Console.WriteLine("Ingrese el nuevo punteo del alumno");
-                    string nuevopunteo = Console.ReadLine();
-                    Console.WriteLine("");
-                    Guardar(nuevonombre, nuevoid, nuevopunteo);
-                }
-                else
-                {
-                    Guardar(datos[0], datos[1], datos[2]);
-                }
-            }
+                Console.WriteLine("Ingrese el ID del alumno que desea modificar");
+                string id = Console.ReadLine();
+                string[] lineas = File.ReadAllLines(filePath);
+                File.WriteAllText(filePath, "");
 
-            Console.WriteLine("Datos del alumno con ID " + id + " modificados con éxito.");
+                foreach (string linea in lineas)
+                {
+                    string[] datos = linea.Split(',');
+                    if (datos[1] == id)
+                    {
+                        Console.WriteLine("Ingrese el nuevo ID del alumno");
+                        string nuevoid = Console.ReadLine();
+                        Console.WriteLine("Ingrese el nuevo nombre del alumno");
+                        string nuevonombre = Console.ReadLine();
+                        Console.WriteLine("Ingrese el nuevo punteo del alumno");
+                        string nuevopunteo = Console.ReadLine();
+                        Console.WriteLine("");
+
+                        Guardar(nuevonombre, nuevoid, nuevopunteo);
+                    }
+                    else
+                    {
+                        Guardar(datos[0], datos[1], datos[2]);
+                    }
+                }
+
+                Console.WriteLine("Datos del alumno con ID " + id + " modificados con éxito.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error al modificar datos: {ex.Message}");
+            }
         }
 
         static void LeerListado()
         {
-            StreamReader leer = new StreamReader(filePath);
-            string linea = "";
-            while (linea != null)
+            try
             {
-                linea = leer.ReadLine();
-                if (linea != null)
+                StreamReader leer = new StreamReader(filePath);
+                string linea = "";
+                while (linea != null)
                 {
-                    string[] datos = linea.Split(',');
-                    Console.WriteLine("Nombre: " + datos[0]);
-                    Console.WriteLine("ID: " + datos[1]);
-                    Console.WriteLine("Punteo: " + datos[2]);
-                    Console.WriteLine(" ");
+                    linea = leer.ReadLine();
+                    if (linea != null)
+                    {
+                        string[] datos = linea.Split(',');
+                        Console.WriteLine("Nombre: " + datos[0]);
+                        Console.WriteLine("ID: " + datos[1]);
+                        Console.WriteLine("Punteo: " + datos[2]);
+                        Console.WriteLine(" ");
+                    }
                 }
+                leer.Close();
             }
-            leer.Close();
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error al leer listado: {ex.Message}");
+            }
         }
     }
 }
